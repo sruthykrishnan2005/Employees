76d1a7acb407238b07b3c6665a80431d4e76d2a7
sruthykrishnan2005 <sruthykrishnan515@gmail.com>
2024-10-24 11:35:38 +0530

    employeeadded

--- my_emp_management_app/src/component/AddTask.jsx
+++ my_emp_management_app/src/component/AddTask.jsx
@@ -1,51 +1,114 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React,{useState} from "react";
+import axios from "axios";
 
-const AddTask = () => {
-    const [name, setName] = useState('');
-    const [address, setAddress] = useState('');
+ const AddDetails=()=>{
+    const[name,addName]=useState('');
+    const [address,addAddress]=useState('');
+    const [position,addPos]=useState('');
+    const [salary,addSalry]=useState('');
+    const [experiance,addExpr]=useState('');
+    const [phone,addPhone]=useState('');
+    const [email,addEmail]=useState('');
+    const [empid,addEmpid]=useState('');
 
-    const handleSubmit = (e) => {
+
+    const handelSubmit =(e)=>{
+        console.log('add form');
+        
         e.preventDefault();
-        axios.post('https://dummyjson.com/users/',{ name, address })
-            .then(response=>{
-                console.log(response.data);
-                setName('');
-                setAddress('');
-            })
-            .catch(error => console.log(error));
+        axios.post('https://aiswarya2325.pythonanywhere.com/employemanagement/employees/',{name,address,position,salary,experiance,phone,email,empid})
+        .then (response=>{
+            console.log(response.data);
+            addName('');
+            addAddress('');
+            addPos('');
+            addSalry('');
+            addExpr('')
+            addPhone('')
+            addEmail('');
+            addEmpid('')
+        })
+        .catch(error=> console.log(error));
     }
+     return(
+        <div className="container">
+            <form onSubmit={handelSubmit}>
+                <h2 className='text-info'><u>Add Details</u></h2>
+                <div>
+                    <label className="text-dark">Name</label>
+                    <input 
+                        className="form-control"
+                        type="text"
+                        value={name}
+                        onChange={(e)=> addName(e.target.value)}
+                    />
+                </div>
+                <div className="mt-2">
+                    <label>Address</label>
+                    <textarea
+                        className="form-control"
+                        value={address}
+                        onChange={(e)=> addAddress(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label className="text-dark">Position</label>
+                    <input 
+                        className="form-control"
+                        type="text"
+                        value={position}
+                        onChange={(e)=> addPos(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label className="text-dark">Salary</label>
+                    <input 
+                        className="form-control"
+                        type="number"
+                        value={salary}
+                        onChange={(e)=> addSalry(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label className="text-dark">Experience</label>
+                    <input 
+                        className="form-control"
+                        type="text"
+                        value={experiance}
+                        onChange={(e)=> addExpr(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label className="text-dark">Phone No.</label>
+                    <input 
+                        className="form-control"
+                        type="number"
+                        value={phone}
+                        onChange={(e)=> addPhone(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label className="text-dark">Email</label>
+                    <input 
+                        className="form-control"
+                        type="email"
+                        value={email}
+                        onChange={(e)=> addEmail(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label className="text-dark">EmpId</label>
+                    <input 
+                        className="form-control"
+                        type="number"
+                        value={empid}
+                        onChange={(e)=> addEmpid(e.target.value)}
+                    />
+                </div>
 
-    return(
-        <div className='container'>
-        <form onSubmit={handleSubmit}>
-            <h2 className='text-info'><u>Add Task</u></h2>
-
-            <div>
-                <label className='text-dark'>Name</label>
-                <input
-                    className='form-control'
-                    type="text"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
-            </div>
-            <div className='mt-2'>
-                <label>Address</label>
-                <textarea
-                    className='form-control'
-
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}
-                />
-            </div>
-            <button type="submit" className='btn btn-success mt-3'>Add Emp</button>
-        </form>
+                    <button type="submit" className="btn btn-success mt-3">Add</button>
+            </form>
         </div>
-    );
-};
-
-export default AddTask;
-
-
-
+     )
+ }
+ export default AddDetails
--- my_emp_management_app/src/component/TaskList.jsx
+++ my_emp_management_app/src/component/TaskList.jsx
@@ -1,47 +1,90 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React,{useState,useEffect} from "react";
+import axios from 'axios'
 
-const TaskList = () => {
-    const [tasks, setEmp]=useState([]);
+
+const TaskList=()=>{
+    const [employs,setEmp]=useState([]);
     const [editing,setEditing]=useState(false);
-    const [currentTask, setCurrentTask]=useState({id:null, name:'', address:'', position:'', salary:'', experience:'', phno:'', email:'', emailid:''});
+    const [currentEmp, setCurrentEmp]= useState({id:null,name:'',address:'',position:'',salary:'',experience:'',phno:'',email:'',empid:''});
 
-    useEffect(() =>{
-        axios.get('https://dummyjson.com/api/users/')
-        .then(response => setTasks(response.data))
+    useEffect(()=>{
+        axios.get('https://aiswarya2325.pythonanywhere.com/employemanagement/employees/')
+        .then(response => setEmp(response.data))
         .catch(error => console.log(error));
     },[]);
-
-
+const editDetails=(dt1)=>{
+    setEditing(false);
+    setCurrentEmp(dt1);
+    
+    axios.get(`https://aiswarya2325.pythonanywhere.com/employemanagement/employees/${id}`,updat)
+}
+    
     return(
-        <div className='container mt-3'>
-            <h2>Employee</h2>
-            <table className='table table-bordered  <td>{task.id}</td>table-hover'>
-                {tasks.map(task => (
-                    <tr key={task.id}>
-                        <td>{task.id}</td>
-                        <td>{task.name}</td>
-                        <td>{task.address}</td>
-                        <td>{task.position}</td>
-                        <td>{task.salary}</td>
-                        <td>{task.experience}</td>
-                        <td>{task.phno}</td>
-                        <td>{task.email}</td>
-                        <td>{task.emailid}</td>
-                        <td><button className='btn btn-warning px-3' onClick={() => editTask(task)}>Edit</button></td>
-                        <td><button className='btn btn-danger' onClick={() => deleteTask(task.id)}>Delete</button></td>
+        <div className="container mt-3">
+            <h2>Employee Table</h2>
+            <table className="table table-bordered table-hover">
+                {employs.map(emp => (
+                    <tr key={emp.id}>
+                        <td>{emp.name}</td>
+                        <td>{emp.address}</td>
+                        <td>{emp.position}</td>
+                        <td>{emp.salary}</td>
+                        <td>{emp.experience}</td>
+                        <td>{emp.email}</td>
+                        <td>{emp.empid}</td>
+                        <td><button className="btn btn-warning px-3" onClick={() =>editDetails(emp)}>Edit</button></td>
+                        <td><button className="btn btn-danger" onClick={() =>deleteTask(task.id)}>Delete</button></td>
                     </tr>
                 ))}
             </table>
-            {/* {editing ? (
+            {editing ?(
                 <EditTaskForm
-                    currentTask={currentTask}
-                    updateTask={updateTask}
+                 currentTask={currentEmp}
+                 updateTask={updateDetails}
                 />
-            ) : null} */}
+            ):null}
+
         </div>
+    );
+};
+const EditTaskForm = ({ currentEmp, updateDetails}) => {
+    const [employs, setEmp] = useState(currentEmp);
+
+    const handleInputChange =(e) => {
+        const { name,value } =e.target;
+        setEmp({ ...employs, [name]:value});
+    };
+
+    const handleSubmit =(e) => {
+        e.preventDefault();
+        updateDetails(employs.id, employs);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Edit Task</h2>
+            <div>
+                <label>Name</label>
+                <input
+                    type="text"
+                    name="name"
+                    value={task.name}
+                    onChange={handleInputChange}
+                />
+            </div>
+            <div>
+                <label>address</label>
+                <textarea
+                    name="address"
+                    value={employs.address}
+                    onChange={handleInputChange}
+                />
+            </div>
+            <button type="submit">Update Task</button>
+        </form>
     );
 };
 
 
-export default TaskList;
+export default TaskList
+    
